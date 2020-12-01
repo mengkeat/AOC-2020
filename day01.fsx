a1ff495cc25000b1e8b316ac5eea107726107f15
Christopher Tay <christopher.tay@vidimensions.com>
2020-12-01 15:51:31 +0800

    Another minor one

--- day01.fsx
+++ day01.fsx
@@ -1,9 +1,8 @@
 open System.IO
 
-let s = File.ReadAllLines( __SOURCE_DIRECTORY__ + "/day01.txt" );;
-let dat = Array.map (fun x-> int x) s;;
+let dat = File.ReadAllLines( __SOURCE_DIRECTORY__ + "/day01.txt" ) |> Array.map int;;
 
-let pairs = [for x in 0..dat.Length-1 do for y in x..dat.Length-1 -> (dat.[x], dat.[y])]
+let pairs = [for x in 0..dat.Length-2 do for y in x+1..dat.Length-1 -> (dat.[x], dat.[y])]
 let part1 = (List.filter (fun (x,y) -> x+y=2020 ) pairs)
                 |> (fun x -> 
                    match x.Head with
